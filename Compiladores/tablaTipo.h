51a59706d2f7add42875d469fecae6e8d68ab53a
Odisseo11 <54052651+Odisseo11@users.noreply.github.com>
2020-06-25 21:42:03 -0500

    Update tablaTipo.h

--- Compiladores/tablaTipo.h
+++ Compiladores/tablaTipo.h
@@ -11,35 +11,6 @@
 	Fecha última versión: 13 de junio de 2020
 */
 
-
-#include <stdio.h>
-#include "tablaSimbol.h"
-#include "pilaTablaSimbol.h"
-#include "tablaTipo.h"
-#include "pilaTablaTipo.h"
-
-int main(int argc, char *argv[]){
-    pilaTipos *ptt = crearPilaTipos();
-    pilaSimbolos *pts = crearPilaSimbolos();
-    insertarTablaTipo(ptt,crearTablaTipo()); //Insertar tabla de tipos y crearla
-    insertarTablaTipo(ptt,crearTablaTipo());
-    printf("%d\n",insertarTipo(getTipoCima(ptt),crearTipoNativo(1,"int",4,1)));
-    printf("%d\n",insertarTipo(getTipoCima(ptt),crearTipoNativo(2,"int",4,2)));
-    printf("%d\n",insertarTipo(getTipoCima(ptt),crearTipoNativo(3,"float",4,3)));
-    printTablaTipo(getTipoCima(ptt));
-    sacarTablaTipos(ptt);
-    insertarTablaSimbolo(pts, crearTablaSimbolo());
-    insertarTablaSimbolo(pts, crearTablaSimbolo());
-    printf("%d\n",insertar(getSimboloCima(pts),crearSimbolo("1",1,0,"int")));
-    printf("%d\n",insertar(getSimboloCima(pts),crearSimbolo("2",2,4,"floar")));
-    printf("%d\n",insertar(getSimboloCima(pts),crearSimbolo("2",3,8,"array")));
-    printTablaSimbolos(getSimboloCima(pts));
-    sacarTablaTipos(pts);
-    return 0;
-}
-
-
-
 #ifndef TABLATIPO_H_INCLUDED
 #include <stdbool.h>
 #include "tablaSimbol.h"
@@ -61,29 +32,28 @@ typedef struct _tipo1 tipo1;
 struct _tipo1{
     int id;
     char nombre [10];
-    tipoBase tb;
+    tipoBase* tb;
     int tamBytes;
     int numElem;
-    tipo *next;
+    tipo1 *next; 
 };
 
 typedef struct _tablaTipo tablaTipo;
 struct _tablaTipo{
-    tipo *root;
+    tipo1 *root;
     int num;
     tablaTipo *next;
 };
 
 /* Retorna un apuntador a una variable type */
 tipo* crearTipoPrimitivo(int id);
-tipo* crearTipoStruct(tablaTipo* estructura); 
-tipoBase* crearATipo(bool is_struct, tipo* tipo_Base);
+tipo* crearTipoStruct(tablaSimbolo* estructura); 
+tipoBase* crearATipo(bool is_struct, tipo tipo_Base);
 tipo1* crearTipoArray(int id, char* nombre, tipoBase* tb, int size, int num_elem);
 tipo1* crearTipoNativo(int id, char* nombre, tipoBase* tb, int size);
-tipo1* crearTipo(char* nombre, int dim, int tipo, int numElem, bool est, tablaSimbolo* estructura);
+tipo1* crearTipo(char* nombre, int dim, int tipon, int numElem, bool est, tablaSimbolo* estructura);
 /* Borra type, libera memoria */
 void borrarTipo(tipo1* t);
-
 /*Crea tabla de tipos*/
 tablaTipo* crearTablaTipo();
 
