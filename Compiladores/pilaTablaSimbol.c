df91a3a6e1dbc073b1cb82b85f25aa0ae071d777
Odisseo11 <54052651+Odisseo11@users.noreply.github.com>
2020-06-25 21:37:24 -0500

    Update pilaTablaSimbol.c

--- Compiladores/pilaTablaSimbol.c
+++ Compiladores/pilaTablaSimbol.c
@@ -1,40 +1,3 @@
-/*
-	Equipo de trabajo:
-	-Álvarez González Ian 
-	-García Oviedo Jaasiel Osmar
-	-López López Ulysses
-	-Domínguez Cisneros Alexis Saúl
-
-	Materia: Compiladores
-	Grupo: 01
-	Semestre: 2020-1
-	Fecha última versión: 13 de junio de 2020
-*/
-#include <stdio.h>
-#include "tablaSimbol.h"
-#include "pilaTablaSimbol.h"
-#include "tablaTipo.h"
-#include "pilaTablaTipo.h"
-
-int main(int argc, char *argv[]){
-    pilaTipos *ptt = crearPilaTipos();
-    pilaSimbolos *pts = crearPilaSimbolos();
-    insertarTablaTipo(ptt,crearTablaTipo()); //Insertar tabla de tipos y crearla
-    insertarTablaTipo(ptt,crearTablaTipo());
-    printf("%d\n",insertarTipo(getTipoCima(ptt),crearTipoNativo(1,"int",4,1)));
-    printf("%d\n",insertarTipo(getTipoCima(ptt),crearTipoNativo(2,"int",4,2)));
-    printf("%d\n",insertarTipo(getTipoCima(ptt),crearTipoNativo(3,"float",4,3)));
-    printTablaTipo(getTipoCima(ptt));
-    sacarTablaTipos(ptt);
-    insertarTablaSimbolo(pts, crearTablaSimbolo());
-    insertarTablaSimbolo(pts, crearTablaSimbolo());
-    printf("%d\n",insertar(getSimboloCima(pts),crearSimbolo("1",1,0,"int")));
-    printf("%d\n",insertar(getSimboloCima(pts),crearSimbolo("2",2,4,"floar")));
-    printf("%d\n",insertar(getSimboloCima(pts),crearSimbolo("2",3,8,"array")));
-    printTablaSimbolos(getSimboloCima(pts));
-    sacarTablaTipos(pts);
-    return 0;
-}
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -55,18 +18,18 @@ pilaSimbolos* crearPilaSimbolos()
 
 void borrarPilaSimbolos(pilaSimbolos* ps) 
 {
-    pilaSimbolos* aux = malloc(sizeof(pilaSimbolos));	
+    tablaSimbolo* aux;
     if(ps){
         
         while(ps->root){
-          aux = ps->root;
-          ps->root = ps->root->next;
-          free(aux);
+            aux = ps->root;
+            ps->root = ps->root->next;
+            free(aux);
         }
         free(ps);
-  }else{
+    }else{
     printf("No existe la pila de tabla de simbolos\n");
-  }
+    }
 }
 
 tablaSimbolo* getSimboloCima(pilaSimbolos* ps) 
@@ -88,7 +51,7 @@ tablaSimbolo* getSimboloFondo(pilaSimbolos* ps)
 void insertarTablaSimbolo(pilaSimbolos* ps, tablaSimbolo* tabla_Simbolo)
 {
     if(ps){    //Si existe la pila
-        if (ps->root == NULL){     //La pila esta vacia
+        if (ps->root == NULL){     //La pila esta vacia 
             ps->root = tabla_Simbolo;
         }else{    
         tablaSimbolo *aux = ps->root;
