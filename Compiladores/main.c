460f0712d64beb7227b3e3570ca7fc7c89daeaea
Odisseo11 <54052651+Odisseo11@users.noreply.github.com>
2020-06-25 21:35:59 -0500

    Update main.c

--- Compiladores/main.c
+++ Compiladores/main.c
@@ -10,28 +10,26 @@
 	Semestre: 2020-1
 	Fecha última versión: 13 de junio de 2020
 */
-#include <stdio.h>
-#include "tablaSimbol.h"
-#include "pilaTablaSimbol.h"
-#include "tablaTipo.h"
-#include "pilaTablaTipo.h"
+#include<stdio.h>
+extern int yyparse();
+extern FILE *yyin;
 
-int main(int argc, char *argv[]){
-    pilaTipos *ptt = crearPilaTipos();
-    pilaSimbolos *pts = crearPilaSimbolos();
-    insertarTablaTipo(ptt,crearTablaTipo());
-    insertarTablaTipo(ptt,crearTablaTipo());
-    printf("%d\n",insertarTipo(getTipoCima(ptt),crearTipoNativo(1,"int",4,1)));
-    printf("%d\n",insertarTipo(getTipoCima(ptt),crearTipoNativo(2,"int",4,2)));
-    printf("%d\n",insertarTipo(getTipoCima(ptt),crearTipoNativo(3,"float",4,3)));
-    printTablaTipo(getTipoCima(ptt));
-    sacarTablaTipos(ptt);
-    insertarTablaSimbolo(pts, crearTablaSimbolo());
-    insertarTablaSimbolo(pts, crearTablaSimbolo());
-    printf("%d\n",insertar(getSimboloCima(pts),crearSimbolo("1",1,0,"int")));
-    printf("%d\n",insertar(getSimboloCima(pts),crearSimbolo("2",2,4,"floar")));
-    printf("%d\n",insertar(getSimboloCima(pts),crearSimbolo("2",3,8,"array")));
-    printTablaSimbolos(getSimboloCima(pts));
-    sacarTablaTipos(pts);
-    return 0;
+int main(int argc, char** argv)
+{
+	FILE *f;
+	if(argc<2)
+	{
+		printf("Faltan argumentos\n");
+		return -1;
+	}
+	f= fopen(argv[1],"r");
+	if(!f)
+	{
+		printf("El archivo %s no se puede abrir\n", argv[1]);
+		return -1;
+	}
+	yyin= f;
+	yyparse();
+	fclose(yyin);
+	return 0;
 }
